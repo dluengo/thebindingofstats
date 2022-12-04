27fda9b45d60c20ca322fe32474542714758dcae
dluengo <olelen@gmail.com>
2022-12-04 10:59:47 +0000

    Mod revamp

--- main.lua
+++ main.lua
@@ -1,201 +1,38 @@
 local json = require("json")
-require("items")
+require("table_to_string")
+require("runstats")
 
-BOI_VERSION = 1
-MOD_NAME = "The Binding of Stats"
+local MOD_NAME = "The Stats of Isaac"
+local data = nil
 
-mod = RegisterMod(MOD_NAME, BOI_VERSION)
-mod.need_init = true
-
--- Determines whether an item, given by its id, is passive or not.
-function mod:__isPassiveItem(id)
-    local item_config = Isaac.GetItemConfig():GetCollectible(id)
-
-    -- For some reason there is a separate type for familiars (i.e. cube of
-    -- meat, brother bobby, dark bum...). So we need to check if the item is
-    -- passive or a familiar.
-    if item_config.Type == ItemType.ITEM_PASSIVE or
-        item_config.Type == ItemType.ITEM_FAMILIAR then
-        return true
-    end
-
-    return false
+function initMod(mod)
 end
 
--- Creates and return a table with the collected passive items of the first
--- character. The table contain strings with the names of the items. Check file
--- items.lua to know more.
-function mod:__getPassiveItems()
-    collected_items = {}
-    for i = ITEM_FIRST_ID, ITEM_LAST_ID do
-        if Isaac.GetPlayer():HasCollectible(i) and mod:__isPassiveItem(i) then
-            collected_items[#collected_items + 1] = getItemNameById(i)
-        end
-    end
-
-    return collected_items
-end
-
--- This sucks a bit. I wanted to put this code in a function as it is used
--- twice. The goal of it was double, compute the heart modifier (the letter)
--- and update the remaining amount of red hp to be computed. Sadly Lua does
--- not support input/output parameters, procedures, parameter passing by
--- reference... call it whatever you want, the idea remains, being able to
--- modify the parameters of a function. So here we make a trick, we return
--- a table with 2 elements, in position one the letter, in position two the
--- remaining HP, the caller of this function most likely will want to update
--- some variable with their remaining HP.
-function mod:__computeRedHP(hp)
-    res = {}
-
-    if hp > 1 then
-        res[1] = "f"
-        res[2] = hp - 2
-    elseif red_hp == 1 then
-        res[1] = "h"
-        res[2] = hp - 1
-    else
-        res[1] = "e"
-        res[2] = hp
-    end
-
-    return res
-end
-
--- Compute the first character's HP and returns a string encoding it.
---
--- Encoding:
---  The general format is (regex): <H>[<HM>]*
---   <H> ::= R: Red heart container
---           S: Soul heart
---           B: Black heart
---           O: bOne heart
---
---   <HM> ::= f: full heart
---          | h: half heart
---          | e: empty heart
---
---  Example:
---   RfRfRfSBOhBOe: 3 full red hearts, 1 soul heart, 1 black heart, 1 bone heart
---                  half-filled, 1 black heart and 1 bone heart empty.
---
--- TODO: Still no support for eternal, rotten, golden hearts or other sort of HP
--- like coins (the keeper) on no HP (the lost).
-function mod:__getCurrentHP()
-    hp_str = ""
-    local player = Isaac.GetPlayer()
-
-    -- Get the number of red-heart containers.
-    local red_containers = player:GetMaxHearts() / 2
-
-    -- Get the number of red health.
-    local red_hp = player:GetHearts()
-
-    -- Iterate over the red-heart containers adding its letter to the resulting
-    -- string and computing also how full they are.
-    while red_containers > 0 do
-        hp_str = hp_str .. "R"
-
-        hp_table = mod:__computeRedHP(red_hp)
-        hp_str = hp_str .. hp_table[1]
-        red_hp = hp_table[2]
-
-        red_containers = red_containers - 1
-    end
-
-    local IS_BLACK = 0x1
-
-    -- This is a bit cumbersome, it is a bit of weird API.
-    --
-    -- At this point the red hearts are already computed and included in the
-    -- resulting string. Now we need to compute soul/black/bone hearts.
-    
-    -- This calculate the number of soul and black hearts (misleading name)
-    local s_b_hearts = player:GetSoulHearts() / 2
-    local bone_hearts = player:GetBoneHearts()
-
-    -- This returns a mask that indicate the position of black hearts (check
-    -- its documentation for further knowledge).
-    local s_b_mask = player:GetBlackHearts()
-    local total_hearts = s_b_hearts + bone_hearts
-
-    for i = 0, total_hearts-1 do
-
-        -- Check what kind of heart this is
-        if player:IsBoneHeart(i) then
-            hp_str = hp_str .. "O"
-
-            hp_table = mod:__computeRedHP(red_hp)
-            hp_str = hp_str .. hp_table[1]
-            red_hp = hp_table[2]
-
-        elseif IS_BLACK & s_b_mask ~= 0 then
-            hp_str = hp_str .. "B"
-            s_b_mask = s_b_mask >> 1
-
+function onGameStarts(isContinue)
+    -- For some reason trying to load the data in initMod() results in function
+    -- mod:HasData() returning false, so as a workaround we do this here.
+    if data == nil then
+        if mod:HasData() then
+            data = json.decode(mod:LoadData())
         else
-            hp_str = hp_str .. "S"
-            s_b_mask = s_b_mask >> 1
+            data = {RUNSTATS_HEADER}
         end
     end
 
-    return hp_str
+    local rs = RunStats:new()
+    rs:GetStatsFromGame(Game())
+
+    data[#data+1] = rs:GetRunValuesAsTable()
 end
 
--- Called the first time a game starts to initialize the mod.
-function mod:initMod()
-    -- Load the file where a previous data was stored or create the table.
-    if mod:HasData() then
-        self.store = json.decode(mod:LoadData())
-    else
-        self.store = {{"Seed", "Character", "HP", "Speed", "Damage", "Tears", "Active Item", "Passive Items"}}
-    end
+function onGameExists()
+    mod:SaveData(json.encode(data))
 end
 
--- This is how the game calculates the tear fire rate.
-function mod:__computeTearDelay(trs)
-    return 30 / (trs + 1)
-end
 
--- Called when a game starts or is continued.
-function mod:onGameStarted(isContinued)
-    -- If this is the first time calling this function initialize the mod.
-    if self.need_init then
-        mod:initMod()
-        self.need_init = false
-    end
+mod = RegisterMod(MOD_NAME, 1)
+initMod(mod)
 
-    -- We are just interested on saving the statistics at the start of a game.
-    if not isContinued then
-        local player = Isaac.GetPlayer()
-        local seed = Game():GetSeeds():GetStartSeedString()
-        local char = player:GetName()
-        local hp = mod:__getCurrentHP()
-        local spd = string.format("%.2f", player.MoveSpeed)
-        local dmg = string.format("%.2f", player.Damage)
-        local trs = string.format("%.2f", mod:__computeTearDelay(player.MaxFireDelay))
-        local a_item = getItemNameById(player:GetActiveItem())
-        local p_items = mod:__getPassiveItems()
-
-        self.store[#self.store + 1] = {seed, char, hp, spd, dmg, trs, a_item, p_items}
-    end
-end
-
--- Called when the Isaac application is about to exit. Stores the statistics
--- in a JSON object under <ISAAC_INSTALL_FOLDER>/data/<MOD_NAME>/save<N>.
-function mod:onGameExit(isGameOver)
-    mod:SaveData(json.encode(self.store))
-end
-
--- I use this function as a quick way to check if the mod loaded or not by
--- printing "Loaded" somewhere on the screen.
-function mod:onRender()
-    Isaac.RenderText("Loaded", 100, 100, 255, 0, 0, 255)
-end
-
--- Entry point
-
-mod:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, mod.onGameStarted)
-mod:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, mod.onGameExit)
---mod:AddCallback(ModCallbacks.MC_POST_RENDER, mod.onRender)
+mod:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, onGameStarts)
+mod:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, onGameExists)
 
---
+++ runstats.lua
@@ -0,0 +1,248 @@
+require("table_to_string")
+local items = require("items")
+
+RUNSTATS_HEADER = {
+    "Seed",
+    "Character",
+    "HP",
+    "Speed",
+    "Damage",
+    "Tears",
+    "Luck",
+    "Passive Items",
+    "Active Item",
+    "Trinket",
+    "Coins",
+    "Bombs",
+    "Keys",
+    "Survived?",
+    "Boss",
+    "Challenge?"
+}
+
+RunStats = {
+    seed = "",
+    char_name = "",
+    hp = "",
+    init_speed = 0,
+    init_damage = 0,
+    init_tears = 0,
+    init_luck = 0,
+    init_items = {},
+    init_active_item = "",
+    init_trinket = "",
+    coins = 0,
+    bombs = 0,
+    keys = 0,
+    survived = false,
+    last_boss = "",
+    is_challenge = false,
+}
+
+function RunStats:new(obj)
+    obj = obj or {}
+    setmetatable(obj, self)
+    self.__index = self
+    return obj
+end
+
+function RunStats:GetStatsFromGame(game)
+    local player = game:GetPlayer(0)
+
+    self.seed = game:GetSeeds():GetStartSeedString()
+    self.char_name = player:GetName()
+    self.hp = self:__computeHP(game)
+    self.init_speed = string.format("%.2f", player.MoveSpeed)
+    self.init_damage = string.format("%.2f", player.Damage)
+    self.init_tears = string.format("%.2f", self:__computeTears(player.MaxFireDelay))
+    self.init_luck = string.format("%.2f", player.Luck)
+    self.init_items = self:__computeItems(game)
+    self.init_active_item = player:GetActiveItem()
+    self.init_trinket = player:GetTrinket(0)
+    self.coins = player:GetNumCoins()
+    self.bombs = player:GetNumBombs()
+    self.keys = player:GetNumKeys()
+
+    if game.Challenge == Challenge.CHALLENGE_NULL then
+        self.is_challenge = false
+    else
+        self.is_challenge = true
+    end
+end
+
+function RunStats:GetEndRunStatsFromGame(game, isGameOver)
+    if isGameOver then
+        self.survived = false
+    else
+        self.survived = true
+        -- TODO: Implement logic to get the last boss killed
+        self.last_boss = ""
+    end
+end
+
+function RunStats:GetRunValuesAsTable()
+    return {
+        self.seed,
+        self.char_name,
+        self.hp,
+        self.init_speed,
+        self.init_damage,
+        self.init_tears,
+        self.init_luck,
+        self.init_items,
+        self.init_active_item,
+        self.init_trinket,
+        self.coins,
+        self.bombs,
+        self.keys,
+        self.survived,
+        self.last_boss,
+        self.is_challenge
+    }
+end
+
+-- Compute the first character's HP and returns a string encoding it.
+--
+-- Encoding:
+--  The general format is (regex): <H>[<HM>]*
+--   <H> ::= R: Red heart container
+--           S: Soul heart
+--           B: Black heart
+--           O: bOne heart
+--
+--   <HM> ::= f: full heart
+--          | h: half heart
+--          | e: empty heart
+--
+--  Example:
+--   RfRfRfSBOhBOe: 3 full red hearts, 1 soul heart, 1 black heart, 1 bone heart
+--                  half-filled, 1 black heart and 1 bone heart empty.
+--
+-- TODO: Still no support for eternal, rotten, golden hearts or other sort of HP
+-- like coins (the keeper) or no HP (the lost).
+function RunStats:__computeHP(game)
+    local IS_BLACK = 0x1
+    local hp_str = ""
+    local player = game:GetPlayer(0)
+
+    -- Get the number of red-heart containers.
+    local red_containers = player:GetMaxHearts() / 2
+
+    -- Get the amount of red health.
+    local red_hp = player:GetHearts()
+
+    -- Iterate over the red-heart containers adding its letter to the resulting
+    -- string and computing also how full they are.
+    while red_containers > 0 do
+        hp_str = hp_str .. "R"
+
+        local hp_table = RunStats:__computeRedHP(red_hp)
+        hp_str = hp_str .. hp_table[1]
+        red_hp = hp_table[2]
+
+        red_containers = red_containers - 1
+    end
+
+    -- At this point the red hearts are already computed and included in the
+    -- resulting string. Now we need to compute soul/black/bone hearts.
+
+    -- This calculates the number of soul and black hearts (misleading name)
+    local s_b_hearts = player:GetSoulHearts() / 2
+
+    -- Get the number of bone-heart containers
+    local bone_hearts = player:GetBoneHearts()
+
+    -- This returns a mask that indicate the position of black hearts (check
+    -- its documentation for further knowledge).
+    local s_b_mask = player:GetBlackHearts()
+    local total_hearts = s_b_hearts + bone_hearts
+
+    for i = 0, total_hearts-1 do
+
+        -- Check what kind of heart this is
+        if player:IsBoneHeart(i) then
+            hp_str = hp_str .. "O"
+
+            local hp_table = RunStats:__computeRedHP(red_hp)
+            hp_str = hp_str .. hp_table[1]
+            red_hp = hp_table[2]
+
+        elseif IS_BLACK & s_b_mask ~= 0 then
+            hp_str = hp_str .. "B"
+            s_b_mask = s_b_mask >> 1
+
+        else
+            hp_str = hp_str .. "S"
+            s_b_mask = s_b_mask >> 1
+        end
+    end
+
+    return hp_str
+end
+
+-- This sucks a bit. I wanted to put this code in a function as it is used
+-- twice. The goal of it was double, compute the heart modifier (the letter)
+-- and update the remaining amount of red hp to be computed. Sadly Lua does
+-- not support input/output parameters, procedures, parameter passing by
+-- reference... call it whatever you want, the idea remains, being able to
+-- modify the parameters of a function. So here we make a trick, we return
+-- a table with 2 elements, in position one the letter, in position two the
+-- remaining HP, the caller of this function most likely will want to update
+-- some variable with the remaining HP.
+function RunStats:__computeRedHP(hp)
+    res = {}
+
+    if hp > 1 then
+        res[1] = "f"
+        res[2] = hp - 2
+    elseif red_hp == 1 then
+        res[1] = "h"
+        res[2] = hp - 1
+    else
+        res[1] = "e"
+        res[2] = hp
+    end
+
+    return res
+end
+
+function RunStats:__computeTears(tears)
+    return 30 / (tears + 1)
+end
+
+function RunStats:__computeItems(game)
+    return {}
+end
+
+-- Creates and return a table with the collected passive items of the first
+-- character. The table contain strings with the names of the items. Check file
+-- items.lua to know more.
+function RunStats:__getPassiveItems(player)
+    local collected_items = {}
+
+    for i = ITEM_FIRST_ID, ITEM_LAST_ID do
+        if player:HasCollectible(i) and RunStats:__isPassiveItem(i) then
+            collected_items[#collected_items + 1] = getItemNameById(i)
+        end
+    end
+
+    return collected_items
+end
+
+-- Determines whether an item, given by its id, is passive or not.
+function RunStats:__isPassiveItem(id)
+
+    -- Accessing the Isaac symbol here is horrible, a lot of coupling.
+    local item_config = Isaac.GetItemConfig():GetCollectible(id)
+
+    -- For some reason there is a separate type for familiars (i.e. cube of
+    -- meat, brother bobby, dark bum...). So we need to check if the item is
+    -- passive or a familiar.
+    if item_config.Type == ItemType.ITEM_PASSIVE or
+        item_config.Type == ItemType.ITEM_FAMILIAR then
+        return true
+    end
+
+    return false
+end
+
